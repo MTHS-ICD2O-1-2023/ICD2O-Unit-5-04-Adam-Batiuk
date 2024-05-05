4f4f6840cc07224a6df1fd4ebf4327b0f151f609
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-05 21:24:12 +0000

    days

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const negative = document.getElementById("negative").checked
+  let randomNumber = Math.floor(Math.random() * 6) + 1
+
+  // process
+  if (negative == true) {
+    randomNumber = randomNumber * (-1)
+    document.getElementById("answer").innerHTML = "The random number is: " + randomNumber
+  } else {
+    document.getElementById("answer").innerHTML = "The random number is: " + randomNumber
+  }
+}
